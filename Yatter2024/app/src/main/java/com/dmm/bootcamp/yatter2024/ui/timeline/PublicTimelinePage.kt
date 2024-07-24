1993f933667b9b90622a4ce9b00d7c7f0620f8ad
matsuuranaoki <matsuura0417@gmail.com>
2024-07-24 10:15:32 +0900

    パブリックタイムラインを実装

---
+++ Yatter2024/app/src/main/java/com/dmm/bootcamp/yatter2024/ui/timeline/PublicTimelinePage.kt
@@ -0,0 +1,28 @@
+package com.dmm.bootcamp.yatter2024.ui.timeline
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.compose.LifecycleEventEffect
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import org.koin.androidx.compose.getViewModel
+
+//class PublicTimelinePage {
+//}
+
+@Composable
+fun PublicTimelinePage(
+    viewModel: PublicTimelineViewModel = getViewModel(),
+) {
+    val uiState by viewModel.uiState.collectAsStateWithLifecycle()
+
+    LifecycleEventEffect(event = Lifecycle.Event.ON_RESUME) {
+        viewModel.onResume()
+    }
+    PublicTimelineTemplate(
+        statusList = uiState.statusList,
+        isLoading = uiState.isLoading,
+        isRefreshing = uiState.isRefreshing,
+        onRefresh = viewModel::onRefresh,
+    )
+}
---
+++ Yatter2024/app/src/main/java/com/dmm/bootcamp/yatter2024/ui/timeline/PublicTimelineViewModel.kt
@@ -0,0 +1,44 @@
+package com.dmm.bootcamp.yatter2024.ui.timeline
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.dmm.bootcamp.yatter2024.domain.repository.StatusRepository
+import com.dmm.bootcamp.yatter2024.ui.timeline.bindingmodel.converter.StatusConverter
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
+
+class PublicTimelineViewModel(
+    private val statusRepository: StatusRepository,
+) : ViewModel() {
+    private val _uiState: MutableStateFlow<PublicTimelineUiState> =
+        MutableStateFlow(PublicTimelineUiState.empty())
+    val uiState: StateFlow<PublicTimelineUiState> = _uiState.asStateFlow()
+
+    private suspend fun fetchPublicTimeline() {
+        val statusList = statusRepository.findAllPublic()
+        _uiState.update {
+            it.copy(
+                statusList = StatusConverter.convertToBindingModel(statusList),
+            )
+        }
+    }
+
+    fun onResume() {
+        viewModelScope.launch { // 1
+            _uiState.update { it.copy(isLoading = true) } // 2
+            fetchPublicTimeline() // 3
+            _uiState.update { it.copy(isLoading = false) } // 4
+        }
+    }
+
+    fun onRefresh() {
+        viewModelScope.launch { // 1
+            _uiState.update { it.copy(isRefreshing = true) } // 2
+            fetchPublicTimeline() // 3
+            _uiState.update { it.copy(isRefreshing = false) } // 4
+        }
+    }
+}
